c30910dc152e809e9c47e4cf8677bcfabe27a540
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-06-30 18:38:45 +0100

    Write tests for branch and cut algorith

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,9 +1,16 @@
 """Tests for exact algorithms for PCTSP"""
 
-# import networkx as nx
+import networkx as nx
 from pctsp import pctsp_branch_and_cut
-
-# from tspwplib import edge_list_from_walk, total_prize, total_cost_networkx
+from tspwplib import (
+    edge_list_from_walk,
+    order_edge_list,
+    total_prize,
+    total_cost_networkx,
+    is_pctsp_yes_instance,
+    vertex_set_from_edge_list,
+    walk_from_edge_list,
+)
 
 
 def test_pctsp_on_suurballes_graph(suurballes_undirected_graph, root):
@@ -11,22 +18,20 @@ def test_pctsp_on_suurballes_graph(suurballes_undirected_graph, root):
     quota = 6
     edge_list = pctsp_branch_and_cut(suurballes_undirected_graph, quota, root)
     assert len(edge_list) > 0
-    # assert (
-    #     total_prize(
-    #         nx.get_node_attributes(suurballes_undirected_graph, "prize"),
-    #         optimal_tour,
-    #     )
-    #     >= quota
-    # )
-    # assert total_cost_networkx(suurballes_undirected_graph, optimal_tour) > 0
+    ordered_edges = order_edge_list(edge_list)
+    optimal_tour = walk_from_edge_list(ordered_edges)
+    assert is_pctsp_yes_instance(
+        suurballes_undirected_graph, quota, root, ordered_edges
+    )
+    assert total_cost_networkx(suurballes_undirected_graph, optimal_tour) == 20
 
 
 def test_pctsp_on_tspwplib(tspwplib_graph, root):
     """Test the branch and cut algorithm on a small, undirected sparse graph"""
-    quota = 6
+    quota = 20
     edge_list = pctsp_branch_and_cut(tspwplib_graph, quota, root)
+    ordered_edges = order_edge_list(edge_list)
     assert len(edge_list) > 0
-
-    # prize_dict = nx.get_node_attributes(tspwplib_graph, "prize")
-    # assert total_prize(prize_dict, optimal_tour) >= quota
-    # assert total_cost_networkx(tspwplib_graph, optimal_tour) > 0
+    optimal_tour = walk_from_edge_list(ordered_edges)
+    assert is_pctsp_yes_instance(tspwplib_graph, quota, root, ordered_edges)
+    assert total_cost_networkx(tspwplib_graph, optimal_tour) > 0
