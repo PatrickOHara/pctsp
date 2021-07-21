6a85cfc29379e689f2391cea3e85a1fddb7f15dc
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-07-21 12:19:21 +0100

    Bug in assigning stats vector

--- src/event_handlers.cpp
+++ src/event_handlers.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "pctsp/event_handlers.hh"
 
 
@@ -31,22 +32,24 @@ SCIP_DECL_EVENTEXEC(NodeEventhdlr::scip_exec) {
     if (SCIPgetStage(scip) >= SCIP_STAGE_SOLVING) {
         ProbDataPCTSP* probdata = dynamic_cast<ProbDataPCTSP*>(SCIPgetObjProbData(scip));
         std::vector<NodeStats> & stats_vector = *probdata->getNodeStats();
-        int num_nodes = SCIPgetNTotalNodes(scip);
+        // int num_nodes = SCIPgetNTotalNodes(scip);
         int stats_size = stats_vector.size();
-        if (stats_size < num_nodes) {
-            // resize stats array
-            stats_vector.resize(num_nodes);
-        }
+
         // get current node and update info about current node
         SCIP_NODE* node = SCIPgetCurrentNode(scip);
         unsigned int node_id = SCIPnodeGetNumber(node);
+
+        if (stats_size < node_id) {
+            // resize stats array
+            stats_vector.resize(node_id);
+        }
         double lower_bound = SCIPnodeGetLowerbound(node);
         double upper_bound = SCIPgetUpperbound(scip);
         unsigned int parent_id = 0;
-        if (node_id > 1) {
-            SCIP_NODE* parent = SCIPnodeGetParent(node);
-            parent_id = SCIPnodeGetNumber(parent);
-        }
+        // if (node_id > 1) {
+        //     SCIP_NODE* parent = SCIPnodeGetParent(node);
+        //     parent_id = SCIPnodeGetNumber(parent);
+        // }
         unsigned int num_sec_disjoint_tour = 0;
         unsigned int num_sec_maxflow_mincut = 0;
         unsigned int num_cost_cover_disjoint_paths = 0;
@@ -55,7 +58,10 @@ SCIP_DECL_EVENTEXEC(NodeEventhdlr::scip_exec) {
         NodeStats node_stats = {lower_bound, node_id, num_sec_disjoint_tour, num_sec_maxflow_mincut,
             num_cost_cover_disjoint_paths, num_cost_cover_shortest_paths, num_cost_cover_steiner_tree,
             parent_id, upper_bound};
-        stats_vector[node_id-1] = node_stats;
+        // if (node_id >=0 && node_id < stats_vector.size())
+            stats_vector[node_id-1] = node_stats;
+        // else
+            // std::cout << "Bad node id: " << node_id << std::endl;
     }
     return SCIP_OKAY;
 }
