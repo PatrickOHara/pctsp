bda211d8016fcb75e45b9dce3e85fcc4ff19ae1c
PatrickOHara <patrick.h.o-hara@warwick.ac.uk>
2021-09-08 15:17:02 +0100

    🐛  Add SCIP dependencies

--- include/pctsp/exception.hh
+++ include/pctsp/exception.hh
@@ -41,10 +41,4 @@ public:
 
 };
 
-class NotImplementedException : public std::logic_error
-{
-public:
-    NotImplementedException () : std::logic_error{"Function not yet implemented."} {}
-};
-
 #endif
